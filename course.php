0c994b4c4f5aa7674f83244956c4938fd2eb9ca6
surwn <ekaterinashuvalova19801309@gmail.com>
2023-05-15 20:16:29 +0300

    dfghjkl

---
+++ course.php
@@ -0,0 +1,35 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "1111";
+    $dbname = "course";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if (isset($_POST['message'])) {
+        $message = $_POST['message'];
+        $info = $message;
+        $massageWithoutLinks = strip_tags($message);
+        preg_match('/#"(\w+)/', $massageWithoutLinks, $matches);
+        $key = $matches[1];
+        $text = preg_replace('/#"(\w+)/','', $massageWithoutLinks);
+        $sql = "INSERT INTO SMS VALUES ('$key', '$text')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Сообщение сохраненно.";
+        } else {
+            echo "Ошибка: " . $sql . "<br>" . $conn->error;
+        }
+        
+        if (isset($_GET['hashtag'])) {
+            $hashtag = $_GET['hashtag'];
+            $sql = "SELECT * FROM SMS WHERE message LIKE '%#$hashtag%'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<p>" . $row["info"] . "</p>";
+                }
+            } else {
+                echo "Нет сообщений с таким хэштегом.";
+            }
+        }
+    }
+?>

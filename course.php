1f0df15666028d44ef0434b8b767816917a8f573
surwn <ekaterinashuvalova19801309@gmail.com>
2023-05-16 10:50:05 +0300

    the last version

--- course.php
+++ course.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $username = "root";
-    $password = "1111";
+    $password = "";
     $dbname = "course";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
